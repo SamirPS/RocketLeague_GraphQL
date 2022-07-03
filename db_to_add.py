6551ed9a2fc1b42d416e8b434f36d638e129917c
SamirPS <akariohsamir@yahoo.com>
2022-07-03 17:08:42 +0200

    add some team in db

--- db_to_add.py
+++ db_to_add.py
@@ -1,4 +1,6 @@
 from app import db
+from test_scrap import *
+
 db.drop_all()
 db.session.commit()
 
@@ -8,21 +10,37 @@ from api.models import Region
 
 EU=Region(name="EU")
 NA=Region(name="NA")
+OCE=Region(name="OCE")
+SAM=Region(name="SAM")
+MENA=Region(name="MENA")
+APACN=Region(name="APAC N")
+APACS=Region(name="APAC S")
+SSA=Region(name="SSA")
 
 db.session.add(EU)
 db.session.add(NA)
+db.session.add(OCE)
+db.session.add(SAM)
+db.session.add(MENA)
+db.session.add(APACN)
+db.session.add(APACS)
+db.session.add(SSA)
+
 
 db.session.commit()
 
 from api.models import Team
-Vitality = Team(region="EU",name="Vitality",player_one="Kaydop",player_two="Alpha54",player_three="Radosin",player_sub="FairyPeak",coach="Ferra")
-Moist = Team(region="EU",name="Moist Esports",player_one="Joyo",player_two="Vatira",player_three="Rise",player_sub="",coach="Noah")
+EU_Team=Get_EU_Teams()
+
+for i in EU_Team:
+    x=Team(region="EU",name=i["name"],player_one=i["1"],player_two=i["2"],player_three=i["3"],player_sub=i["sub"],coach=i["coach"])
+    db.session.add(x)
+    db.session.commit()
 
 G2 = Team(region="NA",name="G2 Esports",player_one="JKnaps",player_two="Chicago",player_three="Atomic",player_sub="",coach="Satthew")
 NRG = Team(region="NA",name="The General NRG",player_one="GarrettG",player_two="jstn",player_three="SquishyMuffinz",player_sub="Musty",coach="Sizz")
 
-db.session.add(Vitality)
-db.session.add(Moist)
+
 db.session.add(G2)
 db.session.add(NRG)
 
