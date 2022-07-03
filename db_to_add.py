1f71577f591bdcabc9a260703ce3a8c0d8da72c4
SamirPS <akariohsamir@yahoo.com>
2022-07-03 17:33:16 +0200

    add some team for all region

--- db_to_add.py
+++ db_to_add.py
@@ -31,20 +31,49 @@ db.session.commit()
 
 from api.models import Team
 EU_Team=Get_EU_Teams()
+NA_Team=Get_NA_Teams()
+OCE_Team=Get_OCE_Teams()
+SAM_Team=Get_SAM_Teams()
+MENA_Team=Get_MENA_Teams()
+APACN_Team=Get_APACN_Teams()
+APACS_Team=Get_APACS_Teams()
+SSA_Team=Get_SSA_Teams()
+
 
 for i in EU_Team:
     x=Team(region="EU",name=i["name"],player_one=i["1"],player_two=i["2"],player_three=i["3"],player_sub=i["sub"],coach=i["coach"])
     db.session.add(x)
     db.session.commit()
 
-G2 = Team(region="NA",name="G2 Esports",player_one="JKnaps",player_two="Chicago",player_three="Atomic",player_sub="",coach="Satthew")
-NRG = Team(region="NA",name="The General NRG",player_one="GarrettG",player_two="jstn",player_three="SquishyMuffinz",player_sub="Musty",coach="Sizz")
+for i in NA_Team:
+    x=Team(region="NA",name=i["name"],player_one=i["1"],player_two=i["2"],player_three=i["3"],player_sub=i["sub"],coach=i["coach"])
+    db.session.add(x)
+    db.session.commit()
 
-
-db.session.add(G2)
-db.session.add(NRG)
-
-db.session.commit()
+for i in OCE_Team:
+    x=Team(region="OCE",name=i["name"],player_one=i["1"],player_two=i["2"],player_three=i["3"],player_sub=i["sub"],coach=i["coach"])
+    db.session.add(x)
+    db.session.commit()
+for i in SAM_Team:
+    x=Team(region="SAM",name=i["name"],player_one=i["1"],player_two=i["2"],player_three=i["3"],player_sub=i["sub"],coach=i["coach"])
+    db.session.add(x)
+    db.session.commit()
+for i in MENA_Team:
+    x=Team(region="MENA",name=i["name"],player_one=i["1"],player_two=i["2"],player_three=i["3"],player_sub=i["sub"],coach=i["coach"])
+    db.session.add(x)
+    db.session.commit()
+for i in APACN_Team:
+    x=Team(region="APAC N",name=i["name"],player_one=i["1"],player_two=i["2"],player_three=i["3"],player_sub=i["sub"],coach=i["coach"])
+    db.session.add(x)
+    db.session.commit()
+for i in APACS_Team:
+    x=Team(region="APAC S",name=i["name"],player_one=i["1"],player_two=i["2"],player_three=i["3"],player_sub=i["sub"],coach=i["coach"])
+    db.session.add(x)
+    db.session.commit()
+for i in SSA_Team:
+    x=Team(region="SSA",name=i["name"],player_one=i["1"],player_two=i["2"],player_three=i["3"],player_sub=i["sub"],coach=i["coach"])
+    db.session.add(x)
+    db.session.commit()
 
 from api.models import Player
 Vatira=Player(name="Axel Touret",nationality="France",born="May 14, 2006",region="EU",status="Active",team="Moist Esports",alternate_IDs="Vati",approx_Total_Winnings="74,978")
